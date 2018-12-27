37c662ce22c78ff50e39f81120f7e9352545ec94
Kurokabe <farid.abdalla.13@gmail.com>
2018-12-27 16:10:32 +0100

    Resolving import errors.

---
+++ ArcOthello_AC/Size.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcOthello_AC
+{
+    class Size
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Size(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+    }
+}
